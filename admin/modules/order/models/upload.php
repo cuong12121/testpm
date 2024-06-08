b77f69eb01616e89c47efa78c5697ac03ef844fb
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-06-08 17:36:42 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -69,7 +69,7 @@
 		
 		function textpdfs($filePath){
 		    
-		    $data = shell_exec('pdftotext -raw '.$filePath.' -');
+		    $data = shell_exec('pdftotext -f 1 -| 1 '.$filePath.' -');
 
 		    $data = preg_replace('/\n/', '', $data);
 
