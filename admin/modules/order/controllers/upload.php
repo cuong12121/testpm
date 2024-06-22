4fd7d29353db687501156a0503de755d961afffb
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-06-22 18:23:07 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -79,7 +79,7 @@
 		    
 		  //  die;
 		    
-		    $path = PATH_BASE.'files/sp1.pdf';
+		    $path = PATH_BASE.'files/aC.pdf';
 		    
 		    $content = $model->textpdfs($path);
 
@@ -98,7 +98,7 @@
 
             $check  = $model->convertContentCheck($content);
 
-            print_r($content);
+            print_r($check);
 
 
 
