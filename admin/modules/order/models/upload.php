02241ee4b3319e24d231c2960b246ab93ee6ba89
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-04 11:24:42 +0700

    update

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -2697,7 +2697,7 @@
                 if(!isset($_SESSION[$this -> prefix.'text0']) || $_SESSION[$this -> prefix.'text0'] == '' || !isset($_SESSION[$this -> prefix.'text1']) || $_SESSION[$this -> prefix.'text1'] == '' || !isset($_SESSION[$this -> prefix.'filter0']) || $_SESSION[$this -> prefix.'filter0'] == 0 || !isset($_SESSION[$this -> prefix.'filter1']) || $_SESSION[$this -> prefix.'filter1'] == 0 || !isset($_SESSION[$this -> prefix.'filter2']) || $_SESSION[$this -> prefix.'filter2'] == 0 ){
                     setRedirect($link,FSText :: _('Vui lòng chọn lọc khung ngày, giờ, kho, sàn trước khi in!'),'error');
                 }
-                $str_ids = '145377, 145379';
+                $str_ids = '215518, 215519';
 
                 // $str_ids = implode(',',$ids);
                 $list = $this->get_records('id IN ('.$str_ids.')','fs_order_uploads');
