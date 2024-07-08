b58d6be8c41c6c3654e816296f4328a7ca5d4351
cuong12121 <cuongdao12121@gmail.com>
2024-07-08 11:09:01 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -289,11 +289,7 @@
 
 				$datas =  preg_replace("/\r?\n/", '', $datas);
 
-				echo $datas;
-
-				die;
-
-				$mau_regex = '/(.*?)Người nhận/s'; // s cho phép . khớp với cả newline
+				$mau_regex = '/(\d+)Người gửi/'; // s cho phép . khớp với cả newline
 
 				if (preg_match($mau_regex, $datas, $matches)) {
 
