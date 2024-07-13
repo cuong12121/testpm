d1e7181ec63db1bdb5fe8bf28461e156da92b116
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-13 17:35:13 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -389,7 +389,7 @@
 
 					$datas = shell_exec('pdftotext  -raw -f '.$i.' -l '.$i.' '.$filePath.' -');
 
-					var_dump($datas);
+					var_dump('pdftotext  -raw -f '.$i.' -l '.$i.' '.$filePath.' -');
 
 					die;
 
