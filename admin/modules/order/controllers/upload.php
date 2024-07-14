a3c80059ec5446864643354f86b99dd94c34f653
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-14 19:42:15 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -248,9 +248,9 @@
 		{
 			// $file =  ['kgh-vnpost_1719639461_cv.pdf','kgh-spx_1719639461_cv.pdf','kgh-ghn_1719639461_cv.pdf'];
 
-			$file =  ['viettel1.pdf', 'viettel2.pdf'];
+			$file =  ['best1.pdf', 'best2.pdf'];
 
-			$platforms =3;
+			$platforms =5;
 
 			$print = $this->dataPDF($file, $platforms);
 
